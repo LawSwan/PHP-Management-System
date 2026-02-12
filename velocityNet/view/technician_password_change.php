dfbb98874f37c12a2c2d6daaff062fcb6b8491ba
c-vial <chase.vial@gmail.com>
2026-02-12 06:22:44 -0500

    Week3 Classes and Db name change

---
+++ velocityNet/view/technician_password_change.php
@@ -0,0 +1,12 @@
+<?php
+// Technician Password Change page.
+// Placeholder page until login is wired up.
+
+require_once("header.php");
+?>
+
+<h2>Technician Password Change</h2>
+
+<p>Placeholder page.</p>
+
+<?php require_once("footer.php"); ?>
